d011773cd3ff9b516410f124937b1f39f37eacd3
Ann Oreng <orengann@gmail.com>
2022-08-25 10:03:43 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const currentUser = "Newton Ombese";
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
 
-console.log(excitedWelcomeMessage)
-console.log(shortGreeting)
+console.log(excitedWelcomeMessage);
+console.log(shortGreeting);
